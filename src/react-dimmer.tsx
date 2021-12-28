c2d167835bbc42d79a7cc6b33b1a020bd6ce908e
RazAbk <RazAb98754@Gmail.com>
2021-12-28 09:26:03 +0200

    fix prettier errors

--- src/react-dimmer.tsx
+++ src/react-dimmer.tsx
@@ -12,13 +12,13 @@ export interface IDimmerProps {
 }
 
 
-export const ReactDimmer = ({ isOpen, exitDimmer, zIndex = 100, opacity = 0.5, blur = 0, saturate = 100, transition = 0.3 }: IDimmerProps) => {
+export const ReactDimmer = ({isOpen, exitDimmer, zIndex = 100, opacity = 0.5, blur = 0, saturate = 100, transition = 0.3,}: IDimmerProps) => {
 
-  const defaultStyle = { zIndex: zIndex.toString(), background: 'rgba(0, 0, 0, 0)', backdropFilter: `blur(0px) saturate(100%)`, transition: `${transition}s`, };
-  const activeStyle = { zIndex: zIndex.toString(), background: `rgba(0, 0, 0, ${opacity})`, backdropFilter: `blur(${blur}px) saturate(${saturate}%)`, transition: `${transition}s`, };
+  const defaultStyle = {zIndex: zIndex.toString(), background: 'rgba(0, 0, 0, 0)', backdropFilter: `blur(0px) saturate(100%)`, transition: `${transition}s`,};
+  const activeStyle = {zIndex: zIndex.toString(), background: `rgba(0, 0, 0, ${opacity})`, backdropFilter: `blur(${blur}px) saturate(${saturate}%)`, transition: `${transition}s`,};
 
   return (<div style={isOpen ? activeStyle : defaultStyle}
-    onClick={() => { exitDimmer(false) }}
+    onClick={() => {exitDimmer(false)}}
     className={`dimmer ${isOpen ? 'dimmer-active' : ''}`}>
   </div>)
 };
