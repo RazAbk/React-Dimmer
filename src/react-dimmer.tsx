f50e1145b16916f26aa73a0c2ea78aa29f536234
RazAbk <RazAb98754@Gmail.com>
2021-12-28 09:49:10 +0200

    fix prettier errors

--- src/react-dimmer.tsx
+++ src/react-dimmer.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import "./react-dimmer.css"
+import * as React from 'react';
+import './react-dimmer.css';
 
 export interface IDimmerProps {
   isOpen: boolean;
@@ -11,14 +11,17 @@ export interface IDimmerProps {
   transition?: number;
 }
 
+export const ReactDimmer = ({ isOpen, exitDimmer, zIndex = 100, opacity = 0.5, blur = 0, saturate = 100, transition = 0.3 }: IDimmerProps) => {
+  const defaultStyle = { zIndex: zIndex.toString(), background: 'rgba(0, 0, 0, 0)', backdropFilter: `blur(0px) saturate(100%)`, transition: `${transition}s` };
+  const activeStyle = { zIndex: zIndex.toString(), background: `rgba(0, 0, 0, ${opacity})`, backdropFilter: `blur(${blur}px) saturate(${saturate}%)`, transition: `${transition}s` };
 
-export const ReactDimmer = ({ isOpen, exitDimmer, zIndex=100, opacity=0.5, blur=0, saturate=100, transition=0.3 }: IDimmerProps) => {
-
-  const defaultStyle = {zIndex: zIndex.toString(), background: "rgba(0, 0, 0, 0)", backdropFilter: `blur(0px) saturate(100%)`, transition: `${transition}s`}
-  const activeStyle = {zIndex: zIndex.toString(), background: `rgba(0, 0, 0, ${opacity})`, backdropFilter: `blur(${blur}px) saturate(${saturate}%)`, transition: `${transition}s`}
-
-  return (<div style={isOpen ? activeStyle : defaultStyle}
-               onClick={() => {exitDimmer(false)}}
-               className={`dimmer ${isOpen ? "dimmer-active" : ""}`}>
-          </div>)
+  return (
+    <div
+      style={isOpen ? activeStyle : defaultStyle}
+      onClick={() => {
+        exitDimmer(false);
+      }}
+      className={`dimmer ${isOpen ? 'dimmer-active' : ''}`}
+    ></div>
+  );
 };
